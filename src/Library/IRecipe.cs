2375f10e416cab7e48d51f3cd907ccb8392c3f06
brunovillero <brunovillero66@gmail.com>
2020-10-28 23:13:02 -0300

    IRecipe

--- src/Library/IRecipe.cs
+++ src/Library/IRecipe.cs
@@ -2,6 +2,12 @@ namespace Full_GRASP_And_SOLID
 {
     public interface IRecipe
     {
+        // Creamos la interfaz IRecipe para que la clase ConsolePrinter y FilePrinter
+        // Dependan de una abstraccion y no de una clase baja como indica el
+        // el principio de inversion de dependencias (DIP).
+        // Tambien el codigo se hace extensible ya que tener una abstraccion
+        // IRecipe permite extender nuestro programa a varios tipos de recetas
+        // y no depender de una clase baja en particular.
         string GetTextToPrint();
     }
 }
