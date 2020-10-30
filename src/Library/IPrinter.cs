bc99b7226a60d1ae9b0341af1abe5e67e85fad5f
Alejandra2431 <69223047+Alejandra2431@users.noreply.github.com>
2020-10-29 23:37:44 -0300

    coemntario de dip

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -3,6 +3,12 @@ namespace Full_GRASP_And_SOLID
 {
     public interface IPrinter
     {
+        /* Modificamos esta interface cambiando el tipo Recipe de recipe por IRecipe para
+        poder aplicar el principio de inversion de dependencias (DIP). IPrinter depende de una
+        abstraccion IRecipe, asi abstrayendose de la clase Recipe. Al depender de una abstraccion 
+        el programa se hace mas movible y deja de ser fragil a los cambios dentro de los que
+        implementa el tipo, ademas deja de ser rigido porque hace al diseño mas facil de cambiar.
+        */
          void PrintRecipe(IRecipe recipe);
     }
 }
